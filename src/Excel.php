143c363caec9d866a17bd923510c7da47cdb2e64
wanggaoyang <836259740@qq.com>
2023-06-25 11:23:26 +0800

    feat:更新注释

--- src/Excel.php
+++ src/Excel.php
@@ -14,7 +14,7 @@ class Excel
     /**
      *  使用示例
      *  $data =[
-     *       ['field1' => 11111,'field2' => 22222],
+     *       ['field1' => 111112,'field2' => 22222],
      *       ['field1' => 111111,'field2' => 222222],
      *  ];
      *  $field = ['标题1' => 'field1','标题2' => 'field2']
